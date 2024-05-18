b5b2669cc41d78e2675f85bddb114b02aca65f08
kelzleo <kofoeyeno33@gmail.com>
2024-05-17 22:53:18 -0700

    second commi

--- app.js
+++ app.js
@@ -5,37 +5,39 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.listen(3000);
-
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 app.use(express.static('public'));
 
 
 
-app.get('/about', (req, res)=> {
-    res.render('About', {title: 'About us'});
+app.get('/about', (req, res) => {
+    res.render('About', { title: 'About us' });
 })
 
 
 
 app.get('/Autism', (req, res) => {
- res.render('Autism', {title: 'Autism'})   
+    res.render('Autism', { title: 'Autism' })
 });
 
-app.get('/Academics', (req, res) =>{
-    res.render('Academics',{title: 'Academics'})
+app.get('/Academics', (req, res) => {
+    res.render('Academics', { title: 'Academics' })
 });
 
 
 
 
 app.get('/', (req, res) => {
-    res.render('home', {title: 'Home'})
+    res.render('home', { title: 'Home' })
 })
 
 app.get('/staffs', (req, res) => {
-    res.render('staffs', {title: 'Staffs'})
+    res.render('staffs', { title: 'Staffs' })
 })
 
 app.get('/contact', (req, res) => {
-    res.render('contact', {title: 'Contact'})
+    res.render('contact', { title: 'Contact' })
 })
